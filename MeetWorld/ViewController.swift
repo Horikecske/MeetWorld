e945435dd8f181721b2f2e498f4cf610dcd07752
Zsombor Nyilas <zs.nyilas@yahoo.com>
2022-04-13 17:43:23 +0200

    Initial Commit

---
+++ MeetWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MeetWorld
+//
+//  Created by Nyilas Zsombor on 2022. 04. 13..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
